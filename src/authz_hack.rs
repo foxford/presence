1f24d3d2ec432eed8d9f5068cf12ce5cd62b2b27
Dmitry Shlagoff <richyannet@yandex.ru>
2023-05-02 11:38:06 +0200

    Fix audience

--- src/authz_hack.rs
+++ src/authz_hack.rs
@@ -1,7 +1,11 @@
 /// Removes `.usr` and `.svc` from audience.
 /// E.g. testing03.usr.example.org => testing03.example.org
 pub fn remove_unwanted_parts_from_audience(audience: &str) -> String {
-    audience.replace(".usr.", ".").replace(".svc.", ".")
+    audience
+        .replace(".usr.", ".")
+        .replace(".svc.", ".")
+        .replace("usr.", "")
+        .replace("svc.", "")
 }
 
 #[cfg(test)]
@@ -23,6 +27,11 @@ mod tests {
         assert_eq!(
             remove_unwanted_parts_from_audience("testing01.usrteacher.org"),
             "testing01.usrteacher.org"
+        );
+
+        assert_eq!(
+            remove_unwanted_parts_from_audience("usr.foxford.ru"),
+            "foxford.ru"
         );
     }
 }
